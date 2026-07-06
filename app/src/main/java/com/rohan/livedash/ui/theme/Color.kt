f86c6482df23c5fc4ce75ffc0a82ebb737311607
Rohan Dora <rohandora007@gmail.com>
2026-07-06 17:11:53 +0530

    add app/src/main/java/com/rohan/livedash/ui/theme/Color.kt

---
+++ app/src/main/java/com/rohan/livedash/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.rohan.livedash.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Accent = Color(0xFFFF2541)
+val AccentDark = Color(0xFFCC1E33)
+val AccentContainer = Color(0x22FF2541)
+
+val Background = Color(0xFF0F0F14)
+val SurfaceCard = Color(0xFF1A1A24)
+val SurfaceElevated = Color(0xFF222230)
+val SurfaceBorder = Color(0xFF2A2A3A)
+val OutlineColor = Color(0xFF303048)
+
+val TextPrimary = Color(0xFFF0F0F8)
+val TextSecondary = Color(0xFFB0B0C8)
+val TextMuted = Color(0xFF6868A0)
+
+val Emerald = Color(0xFF22C55E)
+val Rose = Color(0xFFFF2541)
+val Amber = Color(0xFFF59E0B)
+
+val OnlineGreen = Color(0xFF22C55E)
+val OfflineGray = Color(0xFF6868A0)
